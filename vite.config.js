ae3bb1780346f5b418b6312d14fa3140f50d4f9c
aml-abdelrhman <Aml586651@gmail.com>
2025-09-15 20:08:48 +0300

    fix: correct vite.config.js mode

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/MORK/" : "/", 
-})
-
-
+  base: mode === "production" ? "/MORK/" : "/",
+}))
